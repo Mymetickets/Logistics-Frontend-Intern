9536961d1d806ad3fca33fb02af087252e7ad869
saintsmile001 <saintsmile001@gmail.com>
2025-07-23 16:50:01 +0100

    component created name modification

---
+++ src/components/ui/InputBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <label class="block text-sm">
+    <span class="text-gray-700 dark:text-gray-400">
+      {{ label }}
+    </span>
+    <input
+      :type="type"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="block w-full mt-1 text-sm border border-gray-300 outline outline-2 outline-purple-400 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-300 dark:outline-purple-400 rounded-md px-3 py-2"
+
+    />
+  </label>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: String,
+  placeholder: String,
+  type: {
+    type: String,
+    default: 'text'
+  },
+  modelValue: String
+})
+
+defineEmits(['update:modelValue'])
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import SelectBox from "./components/ui/SelectBox.vue";
+import InputBox from "./components/ui/InputBox.vue";
+import Button from "./components/ui/Button.vue";
+import RadioButton from "./components/ui/RadioButton.vue";
+import CheckBox from "./components/ui/CheckBox.vue";
+import Card from "./components/ui/Card.vue";
+
+createApp(App)
+  .component("SelectBox", SelectBox)
+  .component("InputBox", InputBox)
+  .component("Button", Button)
+  .component("Card", Card)
+  .component("RadioButton", RadioButton)
+  .component("Checkbox", CheckBox)
+  .mount("#app");
